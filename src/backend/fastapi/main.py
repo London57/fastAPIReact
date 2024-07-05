c7791f1baa6274a045849421f5a49aad9e6c047b
london <gleb.yurov.1998@mail.ru>
2024-07-05 18:43:51 +0300

    add CORS on backend

--- src/backend/fastapi/main.py
+++ src/backend/fastapi/main.py
@@ -1,5 +1,7 @@
 import uvicorn
 from  fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from contextlib import asynccontextmanager
 from db.db import create_tables, drop_tables
@@ -11,13 +13,22 @@ from api.routers import all_routers
 async def lifespan(app):
     await create_tables()
     yield
-    await drop_tables()
 
 
 app = FastAPI(
     title='аналог Asana',
     lifespan=lifespan
 )
+
+#CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173'],
+    allow_credentials =True,
+    allow_methods=['GET', 'POST'], 
+    allow_headers=['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
+)
+
 
 for router in all_routers:
     app.include_router(router)
@@ -26,4 +37,4 @@ for router in all_routers:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", reload=True)
+    uvicorn.run(app="main:app", reload=True, port=8080)
